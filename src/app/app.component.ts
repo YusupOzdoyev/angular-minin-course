f05a1f8411e80be7f9996e1b7713ef8fda11868e
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2023-09-20 21:04:40 +0300

    add more functional for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
-import { products as data } from './data/products';
+import { ProductsService } from './services/products.services';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular app';
+  // products: IProduct[] = []
+  loading = false
+  products$: Observable<IProduct[]>
+  term = ''
 
-  products: IProduct[] = data
+  constructor(private productsService: ProductsService) { 
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
+
 }
